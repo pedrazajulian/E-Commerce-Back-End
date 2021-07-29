69602569bf09af68fbf6a56148638f2662700e18
julian <pedrazajulian@hotmail.com>
2021-07-29 19:59:59 -0400

    define columns

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,6 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     // define columns
   },
   {
