eb8fefe10c26ddc1118675ed700b5650e04edaec
julian <pedrazajulian@hotmail.com>
2021-07-29 20:06:09 -0400

    define columns

--- models/Tag.js
+++ models/Tag.js
@@ -7,7 +7,17 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
+  
   {
     sequelize,
     timestamps: false,
